93ae0b01d52e23747ca2231c8528f5d16dbe8385
nadeemjaleel <nadeemjaleel4343@gmail.com>
2024-11-18 17:05:20 +0530

    image loader

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function toggleMenu() {
     burgerMenu.classList.toggle("active");
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImages = document.querySelectorAll(".lazy-load");
+  
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.dataset.src;
+          img.onload = () => img.classList.add("loaded");
+          observer.unobserve(img);
+        }
+      });
+    });
+  
+    lazyImages.forEach((img) => {
+      imageObserver.observe(img);
+    });
+  });
+  
